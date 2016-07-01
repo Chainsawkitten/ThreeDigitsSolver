d373266b3c254b03032dd99741fdbd0d4b40b958
Albin Bernhardsson <catscratchesp@gmail.com>
2016-07-01 23:06:52 +0200

    Get numbers from game.

--- src/main.cpp
+++ src/main.cpp
@@ -16,30 +16,41 @@ unsigned int GetNumber(std::pair<int, int> position) {
     
     // Read text file and get the number.
     std::ifstream file("out.txt");
+    
     unsigned int number;
-    file >> number;
+    if (!(file >> number)) {
+        file.close();
+        return 0;
+    }
     file.close();
     
     return number;
 }
 
 int main() {
-    std::pair<int, int> positions[] = {           {913, 137},
-                                                  {817, 253}, {1010, 253},
-                                                  {720, 369}, {913, 369}, {1106, 369},
-                                                  {817, 484}, {1010, 484},
-                                                  {720, 600}, {913, 600}, {1106, 600},
-                                                  {817, 716}, {1010, 716},
-                                                  {913, 831}              };
+    std::pair<int, int> positions[] = {            {913, 137},
+                                             {817, 253}, {1010, 253},
+                                       {720, 369}, {913, 369}, {1106, 369},
+                                             {817, 484}, {1010, 484},
+                                       {720, 600}, {913, 600}, {1106, 600},
+                                             {817, 716}, {1010, 716},
+                                                   {913, 831}             };
     
     Solver solver;
-    unsigned int numbers[14] = {501, 440, 607, 788, 132, 758, 651, 264, 18, 407, 263, 179, 906, 338};
+    unsigned int numbers[14];
     
     // Instruct user to switch to Three Digits, then wait for them to do so.
     std::cout << "You have 10 seconds to switch to Three Digits and open the level you're currently on." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(10));
     
-    // Test getting number.
-    unsigned int testNumber = GetNumber(positions[0]);
-    std::cout << "Got number: " << testNumber << std::endl;
+    // Get numbers.
+    for (int i=0; i<14; ++i) {
+        numbers[i] = GetNumber(positions[i]);
+        std::cout << positions[i].first << ":" << positions[i].second << " - " << numbers[i] << std::endl;
+    }
+    
+    // Solve.
+    solver.SetNumbers(numbers);
+    solver.Solve();
+    solver.Print();
 }
